e9477d05c7c40ba814ba9203d96d7712dac93983
양아름 <yangareum0130@gmail.com>
2025-05-06 12:45:08 +0900

    feat : 비동기 test 코드

--- src/apis/user.js
+++ src/apis/user.js
@@ -1,4 +1,5 @@
-export default function fetchUser(id, cb) {
+// 콜백함수 테스트
+export function fetchUser(id, cb) {
   setTimeout(() => {
     console.log("wait 0.1 sec.");
     const user = {
@@ -8,4 +9,19 @@ export default function fetchUser(id, cb) {
     };
     cb(user);
   }, 100);
+}
+
+// Promise 테스트
+export function fetchPromiseUser(id) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("wait 0.1 sec.");
+      const user = {
+        id: id,
+        name: "User" + id,
+        email: id + "@test.com",
+      };
+      resolve(user);
+    }, 100);
+  });
 }
--- src/apis/user.test.js
+++ src/apis/user.test.js
@@ -1,12 +1,35 @@
-import fetchUser from "./user";
+import { fetchUser, fetchPromiseUser } from "./user";
 
+// 콜백함수 테스트
 test("fetch a user", (done) => {
   fetchUser(1, (user) => {
+    // 1 통과, 2 실패
     expect(user).toEqual({
       id: 1,
       name: "User1",
       email: "1@test.com",
     });
     done();
+  });
+});
+
+// Promise 테스트 : return문 반드시 추가
+test("fetch a user", () => {
+  return fetchPromiseUser(1).then((user) => {
+    expect(user).toEqual({
+      id: 1,
+      name: "User1",
+      email: "1@test.com",
+    });
+  });
+});
+
+// async/await 테스트
+test("async/await a user", async () => {
+  const user = await fetchPromiseUser(1); // 1성공 2실패
+  expect(user).toEqual({
+    id: 1,
+    name: "User1",
+    email: "1@test.com",
   });
 });
